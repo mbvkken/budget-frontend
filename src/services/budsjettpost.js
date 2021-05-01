d59c8c5d844d0d93a16fa03da4bbaa8b8a53aed6
Kamilla <kamilla_berg@hotmail.com>
2021-05-01 15:00:35 +0200

    budsjettpost fetch funksjoner til backend

--- src/services/budsjettpost.js
+++ src/services/budsjettpost.js
@@ -10,3 +10,24 @@ export function lagBudsjettpost(tittel, sum, fast, budsjettID){
     })
     .then((res) => res.json());
 }
+
+export function redigereBudsjettpost(tittel, sum, fast, budsjettpostID){ 
+    return fetch(`${API_URL}/budsjettpost/${budsjettpostID}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({tittel, sum, fast})
+    })  
+}
+
+export function henteBudsjettposterEtterKat(kategoriID) {
+    return fetch(`${API_URL}/budsjettpost/${kategoriID}`)
+    .then((res) => res.json());
+}
+
+export function sletteBudsjettpost(budsjettpostID) {
+    return fetch(`${API_URL}/budsjettpost/${budsjettpostID}`, {
+        method: 'DELETE'
+    })
+}
